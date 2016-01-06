044b2138fb536df7e90fc4b3b2257eda43e76378
Alan Hamlett <alan.hamlett@gmail.com>
2016-01-06 13:56:07 -0800

    upgrade wakatime-cli core to v4.1.9

--- plugin/packages/wakatime/main.py
+++ plugin/packages/wakatime/main.py
@@ -291,7 +291,7 @@ def send_heartbeat(project=None, branch=None, hostname=None, stats={}, key=None,
     """
 
     if not api_url:
-        api_url = 'https://wakatime.com/api/v1/heartbeats'
+        api_url = 'https://api.wakatime.com/api/v1/heartbeats'
     if not timeout:
         timeout = 30
     log.debug('Sending heartbeat to api at %s' % api_url)
